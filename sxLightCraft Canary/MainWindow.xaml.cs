6fab02d87d81e662f11950660e84da80887c5c9b
sxnvte <sxnate000@gmail.com>
2023-02-17 13:42:22 +0100

    Updated UI

--- sxLightCraft Canary/MainWindow.xaml.cs
+++ sxLightCraft Canary/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace sxLightCraft_Canary
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
